4c672d8ff0ea8b09cac06d49d5be070eda0be059
MaxiB0nng <maxgravgaard@icloud.com>
2025-12-05 09:22:30 +0100

    made settings, we work the valg
    har lavet settings med scale og har lavet om på hvordan selected_valg fungere med bool istedt for str

--- The_night.py
+++ The_night.py
@@ -35,6 +35,11 @@ story_visible = False  #bestemer om text 3 vises eller ej
 startup_show = False #bruger til startup animation 
 story_visible = False
 
+selected_valg_1 = False
+selected_valg_2 = False
+selected_valg_3 = False
+selected_valg_4 = False
+
 
 # Endelig skærmstørrelse beregnes
 startup_sequence = [
@@ -63,70 +68,71 @@ startup_sequence = [
 startup_index = 0
 startup_next_time = 0
 
-scaled_width = screen_width * scale
-scaled_height = screen_height * scale
+def canvas_making():
+    global font, story_canvas, text_canvas, main_canvas, canvas, scaled_width, scaled_height, SCREEN
+    scaled_width = screen_width * scale
+    scaled_height = screen_height * scale
 
-# Skab skærm og indstil ikon
-SCREEN = pygame.display.set_mode((scaled_width, scaled_height))  # Endelig skærmstørrelse
+    # Skab skærm og indstil ikon
+    SCREEN = pygame.display.set_mode((scaled_width, scaled_height))  # Endelig skærmstørrelse
 
-pygame.display.set_caption("The Night")
-the_night_logo = pygame.image.load("img/logo.png").convert_alpha()  # Indlæs dit ikonbillede
-pygame.display.set_icon(the_night_logo)  # Sæt ikonet for vinduet
+    pygame.display.set_caption("The Night")
+    the_night_logo = pygame.image.load("img/logo.png").convert_alpha()  # Indlæs dit ikonbillede
+    pygame.display.set_icon(the_night_logo)  # Sæt ikonet for vinduet
 
-# Opret skrifttype (angiv skrifttype og størrelse)
-font_path = "Pixelon-OGALo.ttf"  # Sti til din skrifttype-fil
-font = pygame.font.Font(font_path, 11)  # Font på 24-punktsstørrelse
+    # Opret skrifttype (angiv skrifttype og størrelse)
+    font_path = "Pixelon-OGALo.ttf"  # Sti til din skrifttype-fil
+    font = pygame.font.Font(font_path, 11)  # Font på 24-punktsstørrelse
 
-# Lav det primære canvas
-canvas = pygame.Surface((screen_width, screen_height))
-canvas.fill(black)
-pygame.draw.rect(canvas, green, (1, 1, 318, 238))  # Grøn kant
+    # Lav det primære canvas
+    canvas = pygame.Surface((screen_width, screen_height))
+    canvas.fill(black)
+    pygame.draw.rect(canvas, green, (1, 1, 318, 238))  # Grøn kant
 
-#main_canvas det der kommer bilder på
-main_canvas_width = 314
-main_canvas_height = 114
-main_canvas = pygame.Surface((main_canvas_width, main_canvas_height))  
-main_canvas.fill(black)  
+    #main_canvas det der kommer bilder på
+    main_canvas_width = 314
+    main_canvas_height = 114
+    main_canvas = pygame.Surface((main_canvas_width, main_canvas_height))  
+    main_canvas.fill(black)  
 
-#story_canvas det der komer text omkring story
-story_canvas_width = 314
-story_canvas_height = 58
-story_canvas = pygame.Surface((story_canvas_width, story_canvas_height))  # Nyt canvas
-story_canvas.fill(black)  
+    #story_canvas det der komer text omkring story
+    story_canvas_width = 314
+    story_canvas_height = 58
+    story_canvas = pygame.Surface((story_canvas_width, story_canvas_height))  # Nyt canvas
+    story_canvas.fill(black)  
 
-#valg
-text_canvas_width = 314
-text_canvas_height = 58
-text_canvas = pygame.Surface((text_canvas_width, text_canvas_height))  # Nyt canvas
-text_canvas.fill(black)  
+    #valg
+    text_canvas_width = 314
+    text_canvas_height = 58
+    text_canvas = pygame.Surface((text_canvas_width, text_canvas_height))  # Nyt canvas
+    text_canvas.fill(black)  
 
+canvas_making()
 
 #laver valg til playerene
 
 valg_x = 4
 
 valg_1_y = 2
-valg_1 = "-"
+valg_1 = "1"
 text_valg_1 = font.render(valg_1, True, green)
 text_valg_b_1 = font.render(valg_1, True, black)
 
 valg_2_y = valg_1_y + 14
-valg_2 = "-"
+valg_2 = "2"
 text_valg_2 = font.render(valg_2, True, green)
 text_valg_b_2 = font.render(valg_2, True, black)
 
 valg_3_y = valg_2_y + 14
-valg_3 = "_"
+valg_3 = "3"
 text_valg_3 = font.render(valg_3, True, green)
 text_valg_b_3 = font.render(valg_3, True, black)
 
 valg_4_y = valg_3_y + 14
-valg_4 = "_"
+valg_4 = "4"
 text_valg_4 = font.render(valg_4, True, green)
 text_valg_b_4 = font.render(valg_4, True, black)
 
-text_selected = 1
-selected_valg = None
 
 #laver story text 
 story_y = 3
@@ -189,17 +195,18 @@ def story_update(text1, text2, text3):
 
 
 def text_valg():
-    global  text_story_3, selected_valg
+    global selected_valg, selected_valg_1, selected_valg_2,selected_valg_3 , selected_valg_4
     
     # Opdater teksten baseret på valget
-    if text_selected == 1:
-        selected_valg = valg_1
-    elif text_selected == 2:
-        selected_valg = valg_2
-    elif text_selected == 3:
-        selected_valg = valg_3
-    elif text_selected == 4:
-        selected_valg = valg_4
+    if valg == 1:
+        selected_valg_1 = True
+    elif valg == 2:
+        selected_valg_2 = True
+    elif valg == 3:
+        selected_valg_3 = True
+    elif valg == 4:
+        selected_valg_4 = True
+
 
 
 def text_redraw():
@@ -268,7 +275,7 @@ def redraw():
         text_canvas.fill(green)
 
     
-    if state == "menu":
+    if state == "menu" or state == "settings" or state == "screen":
         main_canvas.blit(start_front, (image_x, image_y))
 
 
@@ -297,9 +304,9 @@ while running:
                 
                 if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:  # Space/enter tast blev trykke
                     enter_pressed = True
-                    need_redraw = True
                     text_valg()
-                    print("enter pressed")
+                    need_redraw = True
+
 
                 elif event.key == pygame.K_DOWN:
                     valg += 1
@@ -334,14 +341,82 @@ while running:
             # Sequence finished — move to menu
             need_redraw = True
             state = "menu"
+            story_update("Welcome to The Night Of", "December 12", "press enter to continue")
+            valg_update("Continue","Settings","-", "-")
             
 
     if state == "menu":
-        story_update("Welcome to The Night Of", "December 12", "press enter to continue")
-        valg_update("Continue","Settings","-", "-")
+
+        if selected_valg_2:
+            state = "settings"
+            story_update("Settings", "-", "-")
+            valg_update("screen", "music", "credits", "back")
+            selected_valg_2 = False
+
+    #       _____      _   _   _                 
+    #      / ____|    | | | | (_)                
+    #     | (___   ___| |_| |_ _ _ __   __ _ ___ 
+    #      \___ \ / _ \ __| __| | '_ \ / _` / __|
+    #      ____) |  __/ |_| |_| | | | | (_| \__ \
+    #     |_____/ \___|\__|\__|_|_| |_|\__, |___/
+    #                                   __/ |    
+    #                                  |___/     
+
+
+    if state == "settings":
+
+        if selected_valg_1:
+            state = "screen"
+            story_update("The screen is fixed at a 320x240 ratio", f"the screen is  {scaled_height}x{scaled_width}", "-")
+            valg_update(f"scale = {scale}", "scale +", "scale -", "back")
+            selected_valg_1 = False
+
+        if selected_valg_4:
+            state = "menu"
+            story_update("Welcome to The Night Of", "December 12", "press enter to continue")
+            valg_update("Continue","Settings","-", "-")
+            selected_valg_4 = False
     
+    if state == "screen":
+
+        if selected_valg_2:
+            scale += 0.5
+            if scale >= 6:
+                scale = 6
+            canvas_making()
+            story_update("The screen is fixed at a 320x240 ratio", f"the screen is  {scaled_height}x{scaled_width}", "-")
+            valg_update(f"scale = {scale}", "scale +", "scale -", "back")
+            need_redraw = True
+            selected_valg_2 = False
+
+        if selected_valg_3:
+            scale -= 0.5
+            if scale <= 1:
+                scale = 1
+            canvas_making()
+            story_update("The screen is fixed at a 320x240 ratio", f"the screen is  {scaled_height}x{scaled_width}", "-")
+            valg_update(f"scale = {scale}", "scale +", "scale -", "back")
+            need_redraw = True
+            selected_valg_3 = False
+
+        if selected_valg_4:
+            state = "settings"
+            story_update("Settings", "-", "-")
+            valg_update("screen", "music", "credits", "back")
+            need_redraw = True
+            selected_valg_4 = False
+
+#       _____                      
+#      / ____|                     
+#     | |  __  __ _ _ __ ___   ___ 
+#     | | |_ |/ _` | '_ ` _ \ / _ \
+#     | |__| | (_| | | | | | |  __/
+#      \_____|\__,_|_| |_| |_|\___|
+
+
     if need_redraw:
         redraw()
         need_redraw = False
+
 
 pygame.quit()
